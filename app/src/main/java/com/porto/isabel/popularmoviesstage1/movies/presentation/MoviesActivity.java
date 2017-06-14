cff6e1992875180f25d948f35e624be9397ba89b
Isabel Porto <belporto@gmail.com>
2017-06-14 23:19:02 +0100

    Create the recycler view with grid layout and start to show the movies.

--- app/src/main/java/com/porto/isabel/popularmoviesstage1/movies/presentation/MoviesActivity.java
+++ app/src/main/java/com/porto/isabel/popularmoviesstage1/movies/presentation/MoviesActivity.java
@@ -2,6 +2,8 @@ package com.porto.isabel.popularmoviesstage1.movies.presentation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.porto.isabel.popularmoviesstage1.AppApplication;
@@ -11,6 +13,7 @@ import com.porto.isabel.popularmoviesstage1.model.moviedb.Movie;
 import com.porto.isabel.popularmoviesstage1.movies.MoviesContract;
 import com.porto.isabel.popularmoviesstage1.movies.di.DaggerMoviesComponent;
 import com.porto.isabel.popularmoviesstage1.movies.di.MoviesModule;
+import com.porto.isabel.popularmoviesstage1.movies.presentation.adapter.MoviesAdapter;
 
 import java.util.List;
 
@@ -18,8 +21,12 @@ import javax.inject.Inject;
 
 public class MoviesActivity extends AppCompatActivity implements MoviesContract.ViewContract {
 
+    private static final int NUMBER_OF_COLUMNS = 3;
     @Inject
     MoviesContract.PresenterContract mPresenter;
+
+    private RecyclerView mRecyclerView;
+    private MoviesAdapter mMoviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +36,27 @@ public class MoviesActivity extends AppCompatActivity implements MoviesContract.
         DaggerMoviesComponent.builder().appComponent(appComponent).moviesModule(
                 new MoviesModule(this)).build().inject(this);
 
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_movies);
         mPresenter.onCreate();
+
+        //TODO: View injection
+        mRecyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+
+        GridLayoutManager layoutManager
+                = new GridLayoutManager(this, NUMBER_OF_COLUMNS);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mMoviesAdapter = new MoviesAdapter();
+        mRecyclerView.setAdapter(mMoviesAdapter);
+
     }
 
 
     @Override
     public void showMovies(List<Movie> movies) {
         Log.d("TAG", movies.toString());
+        mMoviesAdapter.setData(movies);
     }
 
     @Override
---
+++ app/src/main/java/com/porto/isabel/popularmoviesstage1/movies/presentation/adapter/MoviesAdapter.java
@@ -0,0 +1,62 @@
+package com.porto.isabel.popularmoviesstage1.movies.presentation.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.porto.isabel.popularmoviesstage1.R;
+import com.porto.isabel.popularmoviesstage1.model.moviedb.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by belporto on 14/06/2017.
+ */
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
+
+    private List<Movie> mMovies = new ArrayList<>();
+    private Context mContext;
+
+    @Override
+    public MoviesAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movies_grid_item, parent, false);
+        return new MoviesAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MoviesAdapterViewHolder holder, int position) {
+        Movie movie = mMovies.get(position);
+        //TODO: API KEY
+        String uri = "https://api.themoviedb.org/3" + movie.getPosterPath();
+        Picasso.with(mContext).load(uri).into(holder.mPosterImageView);
+    }
+
+    public void setData(List<Movie> movies) {
+        mMovies = movies;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovies.size();
+    }
+
+    class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        final ImageView mPosterImageView;
+
+        MoviesAdapterViewHolder(View view) {
+            super(view);
+            mPosterImageView = (ImageView) view.findViewById(R.id.movie_poster_image_view);
+        }
+
+    }
+}
